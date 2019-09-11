eb6dd8ea350d3083028c5e46babdf7b2ea58c48c
jolenezheng <jolenezheng18@gmail.com>
2019-09-11 19:05:15 -0400

    second commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,18 @@
 console.log("client side javascript file is loaded!");
+
+fetch("http://puzzle.mead.io/puzzle").then((response) => {
+    response.json().then((data) => {
+        console.log(data);
+    })
+});
+
+// fetch api for client side javascript
+fetch("http://localhost:3000/weather?address=!").then((response) => {
+    response.json().then((data) => { 
+        if (data.error) {
+            return console.log(data.error);
+        }
+        console.log(data.location);
+        console.log(data.forecast);
+    })
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,22 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+    const url = "https://api.darksky.net/forecast/1ed1352be7ddba6d970135e60fd71f62/" + latitude + "," + longitude + "?units=si";
+
+    request({ url: url, json: true }, (error, { body }) => {
+        if (error) {
+            callback(error, undefined);
+        } else if (body.error) {
+            callback(body.error, undefined);
+        } else {
+            const temp = body.currently.temperature;
+            const precPercent = body.currently.precipProbability;
+            callback(undefined, body.daily.data[0].summary +
+                " It is currently " + temp +
+                " degrees out. There is a " + precPercent +
+                "% chance of rain.");
+        }
+    })
+}
+
+module.exports = forecast;
