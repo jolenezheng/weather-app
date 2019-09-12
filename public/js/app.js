610a1adea7a77f82c051e836cd4f6a365fe5e6a8
jolenezheng <jolenezheng18@gmail.com>
2019-09-12 16:50:47 -0400

    removed unnecessary code

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,6 @@
 // When committing changes to git, make sure node_modules aren't including
 // User can re-create it using "npm install"
 // Which uses package.json and package-lock.jason to recreate
-console.log("client side javascript file is loaded!");
 
 fetch("http://puzzle.mead.io/puzzle").then((response) => {
     response.json().then((data) => {
