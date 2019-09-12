eb1c1d527ffd05b37e54eeb44c26f593cf53917e
jolenezheng <jolenezheng18@gmail.com>
2019-09-12 16:46:45 -0400

    Styled weather app

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,6 @@
+// When committing changes to git, make sure node_modules aren't including
+// User can re-create it using "npm install"
+// Which uses package.json and package-lock.jason to recreate
 console.log("client side javascript file is loaded!");
 
 fetch("http://puzzle.mead.io/puzzle").then((response) => {
@@ -6,13 +9,29 @@ fetch("http://puzzle.mead.io/puzzle").then((response) => {
     })
 });
 
-// fetch api for client side javascript
-fetch("http://localhost:3000/weather?address=!").then((response) => {
-    response.json().then((data) => { 
-        if (data.error) {
-            return console.log(data.error);
-        }
-        console.log(data.location);
-        console.log(data.forecast);
-    })
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const message1 = document.querySelector("#message-1");
+const message2 = document.querySelector("#message-2");
+
+weatherForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const location = search.value;
+
+    message1.textContent = "";
+    message2.textContent = "";
+
+    // fetch api for client side javascript
+    fetch("http://localhost:3000/weather?address=" + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                return message1.textContent = data.error;
+            }
+
+            message1.textContent = data.location;
+            message2.textContent = data.forecast;
+
+        })
+    });
 })
